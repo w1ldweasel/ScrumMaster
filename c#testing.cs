a81c70dc28220932f70dad299b008c5d05aa2e22
eddiemaw <eddiemaw@gmail.com>
2022-09-02 14:37:15 +0000

    Update c#testing.cs

--- c#testing.cs
+++ c#testing.cs
@@ -1,3 +1,4 @@
 string query1 = "SELECT count(*) FROM [User] WHERE [Account] = '" + account + "' AND [Password] = '" + password + "'";
 string query2 = String.Format("SELECT count(*) FROM [User] WHERE [Account] = '{0}' AND [Password] = '{1}'", account, password);
 string query3 = 0;
+string query4 = 1;
