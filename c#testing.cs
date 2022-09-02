d115dd049548611dbaf13745d97358a637c1c461
eddiemaw <eddiemaw@gmail.com>
2022-09-02 14:22:27 +0000

    Create c#testing.cs
    Test push

---
+++ c#testing.cs
@@ -0,0 +1,2 @@
+string query1 = "SELECT count(*) FROM [User] WHERE [Account] = '" + account + "' AND [Password] = '" + password + "'";
+string query2 = String.Format("SELECT count(*) FROM [User] WHERE [Account] = '{0}' AND [Password] = '{1}'", account, password);
